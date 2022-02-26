1b589506b68a68ab56d2485b9d501ed5a85ce2aa
Dinh Huy Khanh <khanhdinh141@gmail.com>
2022-02-26 13:41:59 +0700

    change file sql

--- DB_QuanLyThietBiMayTinh.sql
+++ DB_QuanLyThietBiMayTinh.sql
@@ -55,10 +55,12 @@ CREATE TABLE tblHoaDonBan(
 GO
 
 CREATE TABLE tblChiTietHoaDonBan(
-	iMaHD INT NOT NULL,
-	iMaMH INT NOT NULL,
-	iSoLuong INT NOT NULL,
-	fThanhTien FLOAT DEFAULT (0),
+	iMaHD		INT NOT NULL,
+	iMaMH		INT NOT NULL,
+	iSoLuong	INT NOT NULL,
+	fThanhTien	FLOAT DEFAULT (0),
+	iBaoHanh	INT NOT NULL,
+	sGhiChu		NVARCHAR(256),
 	PRIMARY KEY(iMaHD, iMaMH),
 	FOREIGN KEY(iMaHD) REFERENCES tblHoaDonban(iMaHD),
 	FOREIGN KEY(iMaMH) REFERENCES tblMatHang(iMaMH)
@@ -150,8 +152,8 @@ GO
 INSERT INTO tblHoaDonBan(iMaNV)
 VALUES (1)
 GO
-INSERT INTO tblChiTietHoaDonBan(iMaHD, iMaMH, iSoLuong)
-VALUES (2,1,3)
+INSERT INTO tblChiTietHoaDonBan(iMaHD, iMaMH, iSoLuong, iBaoHanh)
+VALUES (1,1,3, 12)
 GO
 SELECT* FROM tblChiTietHoaDonNhap
 SELECT * FROM tblHoaDonNhap
