adce2ec550353e731bf9f97ea7c00a3e56a9a635
quockhanhlsks123 <quockhanhlsls44@gmail.com>
2022-03-19 23:18:57 +0700

    First commit

--- QuanLyThietBiMayTinh/QuanLyThietBiMayTinh/ChiTietHoaDonNhapDTO.cs
+++ QuanLyThietBiMayTinh/QuanLyThietBiMayTinh/ChiTietHoaDonNhapDTO.cs
@@ -12,8 +12,8 @@ namespace QuanLyThietBiMayTinh
         private int iSoLuong;
         private float fDonGia;
 
-        public int IMaMH { get => iMaMH; set => iMaMH = value; }
-        public int ISoLuong { get => iSoLuong; set => iSoLuong = value; }
-        public float FDonGia { get => fDonGia; set => fDonGia = value; }
+        public int IMaMH { get; set; }
+        public int ISoLuong { get; set; }
+        public float FDonGia { get; set; }
     }
 }
